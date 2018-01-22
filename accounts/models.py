4e06dfae5a3456248f5df305bc4fdfcfda18cb72
siavash sadabadi <siavash.sadabadi@gmail.com>
2018-01-23 01:43:02 +0330

    add Account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    user_field = models.CharField(max_length=1024)
+    password_field = models.TextField(blank=True)
+    broken_count = models.PositiveSmallIntegerField(
+        default=0,
+        verbose_name="Broken report count"
+    )
+    seen_count = models.BigIntegerField(default=0)
+    address = models.URLField(
+        max_length=2048,
+        verbose_name="Address in which this account is usable"
+    )
