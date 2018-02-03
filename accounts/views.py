33846f2a025c09684da099604df76b7bb0ec2b5d
siavash sadabadi <siavash.sadabadi@gmail.com>
2018-02-03 07:13:34 +0330

    add Email and Account viewsets

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from accounts.models import Account, Email
+from accounts.serializers import AccountSerializer, EmailSerializer
 
-# Create your views here.
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def perform_create(self, serializer):
+        # todo create/update resource with user or without?
+        if self.request.user.is_anonymous:
+            serializer.save()
+        else:
+            serializer.save(owner=self.request.user)
+
+
+class EmailViewSet(viewsets.ModelViewSet):
+    queryset = Email.objects.all()
+    serializer_class = EmailSerializer
+
+    def perform_create(self, serializer):
+        # todo create/update resource with user or without?
+        if self.request.user.is_anonymous:
+            serializer.save()
+        else:
+            serializer.save(owner=self.request.user)
